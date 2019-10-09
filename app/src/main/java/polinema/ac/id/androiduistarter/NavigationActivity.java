39852e530087f37df54e05923f36ea024be42648
GarisonPB <muhammadgarison99@gmail.com>
2019-10-09 15:34:23 +0700

    Android UI

--- app/src/main/java/polinema/ac/id/androiduistarter/NavigationActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/NavigationActivity.java
@@ -1,16 +1,14 @@
-package polinema.ac.id.androiduistarter.activities;
+package polinema.ac.id.androiduistarter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import polinema.ac.id.androiduistarter.R;
-
-public class MainActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
     }
 }
